c61b06cd2f4b1ea4e29eb680db952afe129231dc
Pavan Kumar Singh <pksX01@gmail.com>
2023-12-18 09:36:56 +0530

    feat: pattern 1

---
+++ Paatterns/pattern1.py
@@ -0,0 +1,13 @@
+# Rectangulat Star pattern
+class Solution:
+    def printSquare(self, n):
+        for i in range(n):
+            for j in range(n):
+                print('*', end=' ')
+            print('')
+
+
+if __name__ == '__main__':
+    N = int(input('Enter a number for which you want to create a rectangular star pattern: '))
+    sol = Solution()
+    sol.printSquare(N)
